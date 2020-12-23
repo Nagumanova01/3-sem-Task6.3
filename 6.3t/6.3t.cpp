839be7a6c7ad2ea48bfbcffeb2c0db035b13a84d
Nagumanova01 <64163169+Nagumanova01@users.noreply.github.com>
2020-12-23 21:58:45 +0300

    Add files via upload

---
+++ 6.3t/6.3t.cpp
@@ -0,0 +1,31 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int main() 
+{
+    int n;
+    cin >> n;
+    map<string, string> a;
+    for (int i = 0; i < n; ++i) 
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+        a.emplace(s1, s2);
+    }
+    string word, wordFound;
+    cin >> word;
+    wordFound = a[word];
+    if (wordFound.empty())
+    {
+        for (auto elem : a)
+        {
+            if (elem.second == word) 
+                cout << elem.first;
+        }
+    } 
+    else cout << wordFound;
+}
